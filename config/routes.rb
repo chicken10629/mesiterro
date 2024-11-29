3c107af3338b1dad6bcd5a843b0c0c9734fe246c
EC2 Default User <ec2-user@ip-172-31-37-25.eu-north-1.compute.internal>
2024-11-29 14:16:20 +0000

    [start] 管理者機能の実装-2

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    #resourceにすると、/:idがURLに含まれなくなる
+    #必要なのはユーザーidとimageidだけなため
+    resources :post_comments, only: [:create, :destroy]
+  end
+  #post_imagesとpost_commentsが親子関係になる
+  #Userモデルを作成した時点で以下が作成
+  #deviseを使用する際、URLにusersを含む
+end
